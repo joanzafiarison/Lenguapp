5a1682e10d7e41adbaddfe7cf072e952dca232b0
DESKTOP-2F87CL7\joanz <joan.zafiarison@do-it.fr>
2021-09-30 01:09:28 +0200

    render html with css and js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const path = require("path")
 const app = express(); 
 
-app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"public","index.html"))
-})
+app.use(express.static(path.join(__dirname,"public")))
 
 app.listen(5000 , () => {
     console.log("server started on port 5000")
