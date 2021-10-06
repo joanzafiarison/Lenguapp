7152f872f46edc218fa6dea395f08c1ccdb42a4d
DESKTOP-2F87CL7\joanz <joan.zafiarison@do-it.fr>
2021-10-06 14:00:06 +0200

    adding a react app to project

--- lenguapp/server/server.js
+++ lenguapp/server/server.js
@@ -18,14 +18,16 @@ const authRouter = require('./routes/authRoutes')
 /*1-Oauth*/ 
 /*2-MAKING DAO implementation*/
 /*3-React serving*/
-/*4-External routing*/
+/*4-External routing ==> OK */
 /*5-3 games  #andYouSay(quizz situationnel) #words(quizz) #buildSentences(mots par briques)*/
 
 app.use(express.static(path.join(__dirname,"public")))
 
 app.use('/', userRouter);
 app.use('/', postRouter);
-app.use('/',apiRouter)
+app.use('/',apiRouter);
+app.use('/', friendRouter); 
+app.use('/',authRouter)
 
 app.listen(5000 , () => {
     console.log("server started on port 5000")
