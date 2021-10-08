29ae38043e5d0345d7c3cd18296933b6e4a86d91
DESKTOP-2F87CL7\joanz <joan.zafiarison@do-it.fr>
2021-10-08 15:36:45 +0200

    rotation principle ok

--- lenguapp/front/src/Container/Train.js
+++ lenguapp/front/src/Container/Train.js
@@ -12,26 +12,52 @@ export default class TrainContainer extends React.Component {
     };
     componentDidMount() {
         axios.get(`http://localhost:5000/exercises/${this.state.exercise_id}`).then((response) => {
-            this.setState({content : response })
+            this.setState({content : response.data })
         })
     }
     
 
     render () {
-        console.log(this.state.content)
+        console.log(this.state.content.words)
+        const {words} = this.state.content
+        
         return(
             <div id ="train_container">
+                <div className="train_meta">
+                    <h1>Mot n°{this.state.cursor + 1}</h1>
+                    <p> Theme : {this.state.content.theme}</p>
+                </div>
+                
+                <div className="train_content">
+                   {words == null ? 
+                        <span>loading...</span> 
+                        :
+                        <div className="words">
+                            <h2>{words[this.state.cursor].word}</h2>
+                            {words[this.state.cursor].words.map((wd)=>(
+                                <li>{wd}</li>
+                            ))}
+                        </div>
+                   }
+                </div>
+                <button disabled = {this.state.cursor == 3 ? true : false} onClick = {() => this.setState({cursor : this.state.cursor + 1})}>suivant !</button>
                
+
             </div>
         )
     };
 }
 
 
-/*{this.state.type === "Words" ?
-<div id="train_content">
-   
-</div> 
-:
-null
-}*/
+
+
+/*{words.map((word_data) => (
+     <>
+    <h2>{word_data.word}</h2>
+        {word_data.words.map((chosen)=>
+            (                    
+                 <button>{chosen}</button>
+            ))
+        }
+    </>
+    ))}*/
