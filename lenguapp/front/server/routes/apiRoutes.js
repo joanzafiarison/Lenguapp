03c166fd593ad71c15cfbbb44d4e1c96a449f7d1
DESKTOP-2F87CL7\joanz <joan.zafiarison@do-it.fr>
2021-10-19 16:06:14 +0200

    course created

--- lenguapp/front/server/routes/apiRoutes.js
+++ lenguapp/front/server/routes/apiRoutes.js
@@ -2,10 +2,18 @@ const express= require("express")
 const router = express.Router()
 const fs = require('fs');
 
+const api = require("../services/apiConnect")
+
 
 const db = require("../config/database")
 const ExerciseSchema = require("../models/exercise")
 const Exercise = db.model("Exercise",ExerciseSchema)
+
+const CourseSchema = require("../models/courses")
+const Course = db.model("Course",CourseSchema)
+
+const DicoSchema = require("../models/dico")
+const Dico = db.model("Dictionnary",DicoSchema)
 
 /*EXERCISES*/ 
 //exercices , make exercise set
@@ -31,10 +39,54 @@ router.get("/exercises/", async function (req,res){
    res.send(exercise)
 })
 
+router.get("/courses/", async function (req,res){
+    let courses = []
+   try {
+    courses = await Course.find()
+   }
+    catch(e) {
+        console.log(e)
+    }
+   res.send(courses)
+})
+
+router.post("/course/", async function (req,res){
+    const {theme,name,level,language,from} = req.body
+    let courses = []
+    let operation = {}
+   try {
+    courses = await Course.find({theme : theme, name : name , level : level, language : language , from : from  })
+    // operation check si unique
+    if (courses.length == 0){
+        operation = await Course.create(req.body)
+    }
+    
+   }
+    catch(e) {
+        console.log(e)
+    }
+   res.send(operation)
+})
+
+router.post("/dico/", async function (req,res){
+    const {word,lang} = req.body
+    let word_info = []
+   try {
+    word_info = await Dico.find({word : word , lang : lang})
+   }
+    catch(e) {
+        console.log(e)
+    }
+   res.send(word_info)
+})
+
 //NLP  get traduction ,japanese :: omae wa mo shindeiru => tu es déjà mort
 router.post("/traduction", function (req,res){
-    const {sentence} = req.body
-    res.send("traduction of \""+sentence+"\" is ")
+    const {TextToTranslate,textToTranslate} = req.body
+    //let msg = api(req.body)
+    //console.log(msg)
+    res.send("traduction ...")
+
     //apiCall to NLP
     //make api call and get results
 })
--- lenguapp/front/server/server.js
+++ lenguapp/front/server/server.js
@@ -1,3 +1,6 @@
+//disponibilité de .env
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors")
 const path = require("path")
