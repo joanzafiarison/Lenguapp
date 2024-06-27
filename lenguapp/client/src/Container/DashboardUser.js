c17175882a302d43d4eee8a7c2cbacb0115e5cd2
joanzafdev <joanzafdev@gmail.com>
2024-06-27 23:04:17 +0200

    Dashboard User personnalization, orientation : delete this page and move content to Train and Learn Page, think about a place to allow personnalized exercise, learning

--- lenguapp/client/src/Container/DashboardUser.js
+++ lenguapp/client/src/Container/DashboardUser.js
@@ -4,8 +4,28 @@ import * as React from "react";
 
 function DashboardUser(){
     return(
-        <div>
-            <h1>DashboardUser</h1>
+        <div className="main_container">
+            <div style={{display:"flex", justifyContent:"space-around"}}>
+            <div>
+                <div className="action_tab">
+                    <h2>S'entrainer</h2>
+                </div>
+                <div className="action_tab">
+                    <h2>Apprendre</h2>
+                </div>
+            </div>
+            <div>
+                <div className="action_tab">
+                    <h2>Chat</h2>
+                </div>
+                <div className="action_tab">
+                    <h2>Partager un exercise</h2>
+                </div>
+                <div className="action_tab">
+                    <h2>Passer premium</h2>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
